6701d90f68bede7fe9c8cb0b3d87cb57edb95a78
tako0910 <tako.oct0910@gmail.com>
2014-02-21 13:59:09 +0900

    Revert "fix felica wip"
    This reverts commit fddfdfc95bd193fb15ded4d031a49403593e841e.

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -5316,11 +5316,7 @@ static struct clk_lookup msm_clocks_8064[] = {
 #ifdef CONFIG_MACH_LGE
 	CLK_LOOKUP("core_clk",		gsbi4_uart_clk.c,	"msm_serial_hsl.0"),
 #else
-#ifdef CONFIG_GSBI5_UARTDM
-	CLK_LOOKUP("core_clk",		gsbi4_uart_clk.c, NULL),
-#else
-	CLK_LOOKUP("core_clk",          gsbi4_uart_clk.c, "msm_serial_hs.1"),
-#endif
+	CLK_LOOKUP("core_clk",		gsbi4_uart_clk.c, "msm_serial_hs.1"),
 #endif
 #ifdef CONFIG_GSBI5_UARTDM
 #ifdef CONFIG_SERIAL_MSM_HS_IMC
@@ -5395,9 +5391,7 @@ static struct clk_lookup msm_clocks_8064[] = {
 	CLK_LOOKUP("iface_clk",		gsbi4_p_clk.c,		"msm_serial_hsl.0"),
 #endif
 	CLK_LOOKUP("iface_clk",		gsbi4_p_clk.c,		"qup_i2c.4"),
-#ifdef CONFIG_GSBI4_UARTDM
 	CLK_LOOKUP("iface_clk",		gsbi4_p_clk.c,	"msm_serial_hs.1"),
-#endif
 	CLK_LOOKUP("iface_clk",		gsbi5_p_clk.c,		"spi_qsd.0"),
 #ifdef CONFIG_GSBI5_UARTDM
 #ifdef CONFIG_SERIAL_MSM_HS_IMC
