97a362a55939206af4622903e8f49af7da06415b
tako0910 <tako.oct0910@gmail.com>
2014-02-21 14:33:13 +0900

    Revert "felica wip wip"
    This reverts commit e044bb3371adbda101ec56f33dfa035896316d56.

--- arch/arm/mach-msm/htc/valente_wx/board-valente_wx.c
+++ arch/arm/mach-msm/htc/valente_wx/board-valente_wx.c
@@ -2951,9 +2951,6 @@ static struct platform_device *common_devices[] __initdata = {
 #ifndef CONFIG_MSM_DSPS
 	&msm8960_device_qup_i2c_gsbi12,
 #endif
-#ifdef CONFIG_GSBI4_UARTDM
-	&msm_device_uart_dm4,
-#endif
 	&msm_slim_ctrl,
 	&msm_device_wcnss_wlan,
 #if defined(CONFIG_QSEECOM)
